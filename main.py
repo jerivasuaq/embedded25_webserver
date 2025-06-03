facd93c25a445b5823f0bfaf7f10d8bcde595ba8
jerivas <jerivas@uaq.edu.mx>
2025-06-02 18:18:53 -0600

    Fix error

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html', person=name)
+    return render_template('index.html')
 
 @app.route("/health")
 def health():
