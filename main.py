60a8c75b960cbda25f2e61567e801c21fed8de64
jerivas <jerivas@uaq.edu.mx>
2025-06-02 18:18:22 -0600

    Rename  to index.html

--- main.py
+++ main.py
@@ -4,13 +4,10 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def main():
-    name = "Pepe"
-
-    return render_template('fb.html', person=name)
+def index():
+    return render_template('index.html', person=name)
 
 @app.route("/health")
 def health():
-    x = 1+1
-    
+   
     return "<p>Server is up and running.</p>"
